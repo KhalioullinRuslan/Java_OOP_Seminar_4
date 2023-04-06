980a8cecdb707dfbf4e8e16f4ee56c5d6e0601c3
unknown <xarus82@mail.ru>
2023-04-06 23:21:39 +0300

    Java_OOP_Seminar_4

---
+++ Warriors/Thief.java
@@ -0,0 +1,11 @@
+package Warriors;
+public class Thief extends Warrior<Knife>{
+    public Thief(String name, int hp, Knife weapon) {
+        super(name, hp, weapon);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
---
+++ Warriors/Weapon.java
@@ -0,0 +1,5 @@
+package Warriors;
+public interface Weapon {
+    int damage();
+    
+    }
